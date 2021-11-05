47a3693f86ec549cb8586abde86268079fed8363
Nicholas Hoban <nickslaptop@Nicholass-MBP.cogeco.local>
2021-11-05 16:08:51 -0400

    getting set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React , { Component}  from 'react';
+import         { render   }  from 'react-dom';
+import './App.css';
+
+function App() {
+  return (
+    <h2>
+      Portfolio project
+    </h2>
+  );
+}
+
+export default App;
